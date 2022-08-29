b2e5f74d7cc23e60805f9ad779b02f86704c4b05
Crowdbotics <team@crowdbotics.com>
2022-08-29 04:18:44 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pg_paddle_selector_36941/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pg_paddle_selector_36941;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "pg_paddle_selector_36941";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pg_paddle_selector_36941",
+  url: "https://pg_paddle_selector_36941.botics.co",
+  api: "https://pg_paddle_selector_36941.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
